440944d63daf52de4a95bf2dd2ea1265a53eacf1
rodolfolazo <rodolfo.lazo@outlook.com>
2024-04-24 14:51:19 +0100

    Corrijo funcionamiento de los formularios cuando se accede a un input

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,7 +1,12 @@
 document.addEventListener("click", function (evt) {
-  if (evt.target != popupForm && evt.target != profile && evt.target != card) {
+  if (
+    evt.target != popupForm &&
+    evt.target != profile &&
+    evt.target != card &&
+    !evt.target.classList.contains("popup__input-text")
+  ) {
     console.log("Has hecho click fuera de la ventana");
-    closePopup();
+    /* closePopup(); */
   } else {
     console.log("Has hecho click en el formulario");
   }
